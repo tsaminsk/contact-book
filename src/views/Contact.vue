74ddc8a0d2d2e185955e309228c547e460c9e9dd
Talai <tsa@tut.by>
2021-08-07 14:15:20 +0300

    add base layout and scss files

---
+++ src/views/Contact.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="contact-page">
+    <h1>This is an page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, onBeforeMount } from 'vue';
+
+export default defineComponent({
+  name: 'Contact',
+  setup() {
+    const title = 'Contact page';
+
+    onBeforeMount(() => {
+      console.log('onBeforeMount');
+    });
+
+    onMounted(() => {
+      console.log('Component is mounted!');
+    });
+
+    return { title };
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="home-page">
+    <h1>{{ title }}</h1>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent } from 'vue';
 
-@Options({
-  components: {
-    HelloWorld,
+export default defineComponent({
+  name: 'Home',
+  setup() {
+    const title = 'Home page';
+    return { title };
   },
-})
-export default class Home extends Vue {}
+});
 </script>
